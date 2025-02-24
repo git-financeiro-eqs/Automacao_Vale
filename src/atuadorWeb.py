0679c6e179c32a8f4aa3cf5d69510f71a6dcee47
Git_Financeiro_EQS <jesse.silva@eqsengenharia.com.br>
2025-02-24 01:49:47 -0300

    Update atuadorWeb.py

--- src/atuadorWeb.py
+++ src/atuadorWeb.py
@@ -10,6 +10,10 @@ from time import sleep
 
 
 class Interagente:
+    """
+    Classe para interagir com páginas web usando Selenium WebDriver.
+    Permite navegar em páginas, preencher campos, esperar por elementos, e outras interações.
+    """
 
     def __init__(self):
         pass
@@ -22,6 +26,20 @@ class Interagente:
 
 
     def interagir_pagina_web(self, xpath, acao, texto="", limitar_espera=False, limitar_retorno=False):
+        """
+        Realiza ações em um elemento da página web, como clicar, escrever ou retornar o próprio elemento.
+
+        Parâmetros:
+            xpath (str): O XPath do elemento a ser encontrado.
+            acao (str): A ação a ser executada. Pode ser "Clicar", "Escrever", "Retornar elemento" ou "Esperar".
+            texto (str, opcional): Texto a ser inserido caso a ação seja "Escrever".
+            limitar_espera (bool, opcional): Limita o tempo de espera para encontrar o elemento.
+            limitar_retorno (bool, opcional): Limita o número de tentativas de encontrar o elemento.
+
+        Retorna:
+            WebElement ou None: Retorna o elemento se a ação for "Retornar elemento", caso contrário retorna None.
+        """
+        
         aux = 0
         while True:
             try:
@@ -49,6 +67,15 @@ class Interagente:
 
 
     def inserir_arquivo(self, xpath, xpath_de_espera, arquivo):
+         """
+        Insere um arquivo no campo de input da página e aguarda até que a ação seja concluída.
+
+        Parâmetros:
+            xpath (str): O XPath do campo onde o arquivo será inserido.
+            xpath_de_espera (str): O XPath do elemento de espera após o arquivo ser inserido.
+            arquivo (str): O caminho do arquivo a ser inserido.
+        """
+        
         self.interagir_pagina_web(xpath, acao="Escrever", texto=arquivo)
         self.interagir_pagina_web(xpath_de_espera, acao="Esperar", limitar_espera=True)
 
@@ -70,6 +97,15 @@ class Interagente:
 
 
     def interagir_javaScript(self, venctos_convertidos, indice, id):
+        """
+        Executa um script JavaScript para preencher um campo de data na página web.
+
+        Parâmetros:
+            venctos_convertidos (list): Lista de datas a serem inseridas.
+            indice (int): Índice da data a ser inserida.
+            id (str): O ID do campo de data na página.
+        """
+        
         date_field = self.driver.find_element(By.ID, id)
         self.driver.execute_script("arguments[0].style.display = 'block';", date_field)
         self.driver.execute_script("""
@@ -80,6 +116,10 @@ class Interagente:
 
 
     def verificar_instabilidade(self, verificar):
+        """
+        Verifica se há instabilidade na página, verificando se os arquivos foram corretamente inseridos.
+        """
+        
         arquivo1_inserido = self.interagir_pagina_web(xpath="/html/body/main/div/div/div/div/form/div/div/div/div[1]/div/div[2]/div[1]/p/span/a", acao="Retornar elemento", limitar_retorno=True)
         arquivo2_inserido = self.interagir_pagina_web(xpath="/html/body/main/div/div/div/div/form/div/div/div/div[1]/div/div[2]/div[2]/p/span/a", acao="Retornar elemento", limitar_retorno=True)
         arquivo3_inserido = self.interagir_pagina_web(xpath="/html/body/main/div/div/div/div/form/div/div/div/div[1]/div/div[2]/div[3]/p/span/a", acao="Retornar elemento", limitar_retorno=True)
